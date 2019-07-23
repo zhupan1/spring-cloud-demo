6d08bd58c5cd65a0d602e03f966aeaa87b0baf18
Pan P Zhu <pan.p.zhu@cn.pwc.com>
2019-07-23 17:56:55 +0800

    删除多余依赖

--- user-service/src/main/java/com/pan/user/service/feign/FeignOrderService.java
+++ user-service/src/main/java/com/pan/user/service/feign/FeignOrderService.java
@@ -2,7 +2,6 @@ package com.pan.user.service.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @Author: Pan P Zhu
